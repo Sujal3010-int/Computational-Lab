11c2ab2214288b7abe99715ac53c6b79a1f72ab4
Sujal3010-int <sujal.sinha@niser.ac.in>
2025-08-16 18:22:05 +0530

    Pi.py

--- Pi.1.py
+++ Pi.1.py
@@ -1,39 +1,32 @@
 from Fun import LCGrandom
 import numpy as np
-import matplotlib.pyplot as plt
-s=Lcg_random()
 
-# define x and y , as a python list
-def Plot(x, y, title='Sample Plot', xlabel='X-axis Label', ylabel='Y-axis Label', file_name='sample_plot.png'):
-    plt.plot(x, y, marker='o', linestyle='-', color='b')
-    plt.title(title)
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-    plt.grid(True)
-    plt.savefig(file_name)
-    plt.show()
-j=[]
-pi=[]
-for i in range(20,20020,20):
-    ntotal=0
-    n1=0
-    j.append(i)
-    while ntotal < i:
-        xt=s.gen()
-        yt=s.gen()
-        r=np.sqrt(xt**2 +yt**2)
+s = LCGrandom(seed=42)
+
+lsti = []
+lstpi = []
+
+for i in range(20, 20020, 20):  
+    ntotal = 0
+    n1 = 0
+    lsti.append(i)
+
+    for _ in range(i):
+        xt = s.generate()
+        yt = s.generate()
+        r = np.sqrt(xt * xt + yt * yt)  # distance from origin
         if r < 1:
-            n1= n1 + 1
-        ntotal = ntotal +1
+            n1 += 1
+        ntotal += 1
 
-    tpi =4*n1/ntotal
-    pi.append(tpi)
-avg=0
-sum=0
-for i in range(len(pi)):
-    sum= sum + pi[i]
-    i=i+1
-avg= sum/len(pi)
+    tpi = 4 * n1 / ntotal
+    lstpi.append(tpi)
+
+
+sum_val = 0
+for val in lstpi:
+    sum_val += val
+avg = sum_val / len(lstpi)
+
 print("The average pi value is:", avg)
-Plot(j,pi)
-
+print("First 10 estimates:", lstpi[:10])
