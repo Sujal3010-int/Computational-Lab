7ac0ca44b89655b08e4715ff03f74c50f1c5a4b1
Sujal3010-int <sujal.sinha@niser.ac.in>
2025-08-16 18:28:39 +0530

    Update Pi.1.py

--- Pi.1.py
+++ Pi.1.py
@@ -1,26 +1,35 @@
 from Fun import LCGrandom
 import numpy as np
+# define x and y , as a python list
+def Plot(x, y, title='Sample Plot', xlabel='X-axis Label', ylabel='Y-axis Label', file_name='sample_plot.png'):
+    plt.plot(x, y, marker='o', linestyle='-', color='b')
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.grid(True)
+    plt.savefig(file_name)
+    plt.show()
 
 s = LCGrandom(seed=42)
 
-lsti = []
-lstpi = []
+li = []
+pi = []
 
 for i in range(20, 20020, 20):  
     ntotal = 0
     n1 = 0
-    lsti.append(i)
+    li.append(i)
 
     for _ in range(i):
-        xt = s.generate()
-        yt = s.generate()
-        r = np.sqrt(xt * xt + yt * yt)  # distance from origin
+        x = s.generate()
+        y = s.generate()
+        r = np.sqrt(x * x + y * y)  # distance from origin
         if r < 1:
             n1 += 1
         ntotal += 1
 
-    tpi = 4 * n1 / ntotal
-    lstpi.append(tpi)
+    t = 4 * n1 / ntotal
+    pi.append(t)
 
 
 sum_val = 0
@@ -30,3 +39,4 @@ avg = sum_val / len(lstpi)
 
 print("The average pi value is:", avg)
 print("First 10 estimates:", lstpi[:10])
+Plot(lsti,lstpi)
