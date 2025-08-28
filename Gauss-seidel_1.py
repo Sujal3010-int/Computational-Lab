a6ef0b0d61e0b931236e5d181ca79872ad6a96f3
Sujal3010-int <sujal.sinha@niser.ac.in>
2025-08-28 17:20:19 +0530

    Update Gauss-seidel_1.py

--- Gauss-seidel_1.py
+++ Gauss-seidel_1.py
@@ -1,11 +1,12 @@
 #Sujal Sinha,2311191
 # Assignment5
 from Fun import *
+#a and b matrices have been swithced when entering the values
 a= read_matrix('asgn0_matA')
 b= read_matrix('asgn0_vecC')
 print(' Solution according to jacobi method', jacobi(a,b,1000))
 print('Solution according to Gauss method', gauss_seidel_nodiag(a,b))
 """Output:
-Solution according to jacobi method [2.5608772550406793, -3.124867350548284, 13.12373540856031, -1.2990449239476476, 1.2519985850725148]
-Solution according to Gauss method [2.560876495148605, -3.1248664748088935, 13.123735229777056, -1.2990444474269947, 1.251998268942399]
+Solution according to jacobi method [2.97916519278387, 2.215599575521754, 0.211284046692607, 0.15231694375663238, 5.715033604527767]
+Solution according to Gauss method [2.9791647917923516, 2.2155999742491046, 0.2112839156849176, 0.15231719966776006, 5.715033463883198]
 """
